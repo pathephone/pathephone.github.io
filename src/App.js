06a5edfa903bfd1534e08e7451f6436d662a1cd6
Максим Ильин <negamaxi@gmail.com>
2018-02-05 02:58:54 +0300

    services comparison table

--- src/App.js
+++ src/App.js
@@ -19,12 +19,48 @@ class App extends Component {
           </a>
         </header>
         <div className='w3-center'>
-          <p className='w3-large'>
-            Imagine Spotify | Google Music | iTunes powered by torrents.
+          <p className='w3-xlarge'>
+            Imagine Spotify | Google Music | Apple Music powered by torrents.
           </p>
-          <p>
-            Yeah, you get it right.
-          </p>
+          <div class="w3-responsive w3-padding">
+            <table class="w3-table-all w3-large">
+              <tr>
+                <th></th>
+                <th>Spotify</th>
+                <th>Google Music</th>
+                <th>Apple Music</th>
+                <th>Pathephone</th>
+              </tr>
+              <tr>
+                <td><b>Quality</b></td>
+                <td>320kbps (Premium)</td>
+                <td>320kbps</td>
+                <td>256kbps</td>
+                <td>no limits, because p2p</td>
+              </tr>
+              <tr>
+                <td><b>Price</b></td>
+                <td>$9.99/mo (Premium)</td>
+                <td>$9,99/mo</td>
+                <td>$9,99/mo</td>
+                <td>no price, because p2p</td>
+              </tr>
+              <tr>
+                <td><b>Library</b></td>
+                <td>~ 30 million tracks</td>
+                <td>~ 40 million tracks</td>
+                <td>~ 45 million tracks</td>
+                <td>any music shared by users</td>
+              </tr>
+              <tr>
+                <td><b>Regional restrictions</b></td>
+                <td><a href='https://support.spotify.com/is/using_spotify/the_basics/full-list-of-territories-where-spotify-is-available/'>yep</a></td>
+                <td><a href='https://support.google.com/googleplay/answer/2843119?hl=en'>yep</a></td>
+                <td><a href='https://support.apple.com/en-us/HT204956'>yep</a></td>
+                <td>nope, because p2p</td>
+              </tr>
+            </table>
+          </div>
         </div>
       </div>
     );
