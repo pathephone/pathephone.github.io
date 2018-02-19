e3f25b7576a6019647134e2d7a6881e1c5cb2feb
Максим Ильин <negamaxi@gmail.com>
2018-02-20 00:15:12 +0300

    fix fake os name

--- src/App/Header/osName.js
+++ src/App/Header/osName.js
@@ -5,4 +5,4 @@ if (navigator.userAgent.indexOf("Mac")!== -1) OSName="MacOS";
 if (navigator.userAgent.indexOf("X11")!== -1) OSName="UNIX";
 if (navigator.userAgent.indexOf("Linux")!== -1) OSName="Linux";
 
-export default 'Linux'
+export default OSName
