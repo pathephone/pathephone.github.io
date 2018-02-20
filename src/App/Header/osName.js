d649fe460a97518b4b911a42fb27e99dc1c555b7
Максим Ильин <negamaxi@gmail.com>
2018-02-21 01:17:40 +0300

    android detected

--- src/App/Header/osName.js
+++ src/App/Header/osName.js
@@ -4,5 +4,6 @@ if (navigator.userAgent.indexOf("Win")!== -1) OSName="Windows";
 if (navigator.userAgent.indexOf("Mac")!== -1) OSName="MacOS";
 if (navigator.userAgent.indexOf("X11")!== -1) OSName="UNIX";
 if (navigator.userAgent.indexOf("Linux")!== -1) OSName="Linux";
+if (navigator.userAgent.indexOf("Android")!== -1) OSName="Android";
 
 export default OSName
