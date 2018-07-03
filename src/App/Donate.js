1862a879bcc0cd499caf217c063b4d0300c89afe
Максим Ильин <negamaxi@gmail.com>
2018-07-03 19:50:59 +0300

    add yandex-wallet donation

--- src/App/Donate.js
+++ src/App/Donate.js
@@ -6,11 +6,15 @@ import BitcoinCashButton from './Donate/BitcoinCashButton'
 export default () => (
   <div className="w3-container" id="about">
     <h3 className="w3-center">Feel free to support</h3>
+    <br />
     <div className="donate w3-center">
       <BitcoinButton />
       <EthereumButton />
       <BitcoinCashButton />
     </div>
     <br />
+    <div className='w3-center'>
+      <iframe title='yandex-wallet' src="https://money.yandex.ru/quickpay/shop-widget?writer=seller&targets=To%20support%20Pathephone%20development&targets-hint=&default-sum=&button-text=14&payment-type-choice=on&hint=&successURL=&quickpay=shop&account=410015891281482" width="423" height="226" frameborder="0" allowtransparency="true" scrolling="no"></iframe>
+    </div>
   </div>
 )
