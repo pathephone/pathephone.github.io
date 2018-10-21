594f09b392ee26afcc1a47eb101a4e8ebcfd4859
Максим Ильин <negamaxi@gmail.com>
2018-10-21 10:49:33 +0300

    Updates

---
+++ precache-manifest.b401cb9672dd2fa9efab1d117f2b6c46.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "76928ce6989e7932e2a33674a14c8559",
+    "url": "/static/media/logo.76928ce6.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "820347570aaea4aba706",
+    "url": "/static/js/main.82034757.chunk.js"
+  },
+  {
+    "revision": "9a9ee4ab113b3c6f0e41",
+    "url": "/static/js/1.9a9ee4ab.chunk.js"
+  },
+  {
+    "revision": "820347570aaea4aba706",
+    "url": "/static/css/main.17df3e92.chunk.css"
+  },
+  {
+    "revision": "eeedcb39fe07049ffd67aec4c3b67888",
+    "url": "/index.html"
+  }
+];
