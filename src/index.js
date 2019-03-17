1f48092ef23f724052fa8e2cbd5cf16a02711b37
Maxim Ilyin <negamaxi@gmail.com>
2019-03-17 16:20:13 +0300

    remove service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './w3.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
